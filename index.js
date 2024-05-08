b16e46c9b65cc95dc652538a111e55c6224b288e
Tariq-Zaidan <106959018+Tariq-Zaidan@users.noreply.github.com>
2024-05-08 15:29:08 +0300

    update css-html

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-/* var diceImg1 = document.querySelector(".dice .img1");
-var diceImg2 = document.querySelector(".dice .img2"); */
 
 var diceImegs= ['./images/dice1.png' , './images/dice2.png' , './images/dice3.png' , 
 './images/dice4.png' , './images/dice5.png' , './images/dice6.png'];
 
-/* diceImg2= ['./images/dice1.png' , './images/dice2.png' , './images/dice3.png' , 
-'./images/dice4.png' , './images/dice5.png' , './images/dice6.png']; */
 
 var randomNumber1 = Math.floor(Math.random()*6);
 var randomNumber2 = Math.floor(Math.random()*6);
@@ -19,10 +15,10 @@ document.querySelector(".dice .img1").setAttribute("src" , diceImg1);
 document.querySelector(".dice .img2").setAttribute("src" , diceImg2);
 
 if (diceImg1 > diceImg2) {
-    document.querySelector("h1").innerHTML = ("Player 1 wins!")
+    document.querySelector("h1").innerHTML = ("🚩Player 1 wins!")
 }
  else if(diceImg2 > diceImg1) {
-    document.querySelector("h1").innerHTML = ("Player 2 wins!")
+    document.querySelector("h1").innerHTML = ("Player 2 wins!🚩")
 }
 else{
     document.querySelector("h1").innerHTML = ("Drow!")
